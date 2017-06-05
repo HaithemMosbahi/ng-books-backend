816a61a05df85d9d3f71a6fcf2bd60ccbbbc0743
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-05 23:39:28 +0100

    initial commit

---
+++ src/main/java/com/demo/books/NgBooksBackendApplication.java
@@ -0,0 +1,12 @@
+package com.demo.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgBooksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgBooksBackendApplication.class, args);
+	}
+}
