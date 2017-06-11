774c5504c3484cf9385512a3afa701c08d6df83b
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-06-11 22:31:19 +0100

    add oauth configuration for google, facebook and github providers

--- src/main/java/com/demo/books/config/SecurityConfiguration.java
+++ src/main/java/com/demo/books/config/SecurityConfiguration.java
@@ -1,63 +1,140 @@
 package com.demo.books.config;
-/*
-import java.util.function.Function;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.UserDetailsRepository;
-import org.springframework.security.authentication.UserDetailsRepositoryAuthenticationManager;
-import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.web.server.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
-import org.springframework.web.server.WebFilter;
-import com.demo.books.service.UserService;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.filter.CompositeFilter;
 
-import reactor.core.publisher.Mono;
-
-*//**
- * Security configuration class 
+/**
+ * Security configuration class
  * 
  * @author hmosbahi
  *
- *//*
-@Configuration
-public class SecurityConfiguration {
+ */
+@EnableOAuth2Client
+@EnableAuthorizationServer
+@Order(6)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	private final OAuth2ClientContext oauth2ClientContext;
+	
+	public SecurityConfiguration(OAuth2ClientContext oauth2ClientContext) {
+		this.oauth2ClientContext = oauth2ClientContext;
+	}
 
-    @Bean
-    UserDetailsRepository userDetailsRepository(UserService userService){
-    	return username -> Mono.justOrEmpty(userService.getUserAuhorities(username))
-    			.flatMapIterable(Function.identity())
-                .map(SimpleGrantedAuthority::new)
-                .collectList()
-                .map(grantedAuthorities -> new User(username, "password", grantedAuthorities));
-    }
-    
-    @Bean
-    ReactiveAuthenticationManager reactiveAuthenticationManager(UserService userService) {
-        return new UserDetailsRepositoryAuthenticationManager(userDetailsRepository(userService));
-    }
 
-    @Bean
-    WebFilter reactive(ReactiveAuthenticationManager manager) throws Exception {
-        HttpSecurity http = HttpSecurity.http();
-        http.authenticationManager(manager).httpBasic();
-        http.authorizeExchange().antMatchers("/**").access(this::authorize);
-        
-        return http.build();
-    }
-    
-    private Mono<AuthorizationDecision> authorize(Mono<Authentication> authentication, AuthorizationContext ctx) {
-        return authentication
-                .flatMapIterable(Authentication::getAuthorities)
-                .any(ga -> ga.getAuthority().equalsIgnoreCase("user"))
-                .map(AuthorizationDecision::new);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated().and().exceptionHandling()
+				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
+				.logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+		// @formatter:on
+	}
+	
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+	
+	private Filter ssoFilter(){
+			CompositeFilter filter = new CompositeFilter();
+			List<Filter> filters = new ArrayList<>();
+			filters.add(ssoFilter(facebook(), "/login/facebook"));
+			filters.add(ssoFilter(google(), "/login/google"));
+			filters.add(ssoFilter(github(), "/login/github"));
+			filter.setFilters(filters);
+			return filter;
+	
+	}
+	
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2ClientAuthenticationProcessingFilter oAuth2ClientAuthenticationFilter = new OAuth2ClientAuthenticationProcessingFilter(path);
+		OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+		oAuth2ClientAuthenticationFilter.setRestTemplate(oAuth2RestTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(),
+				client.getClient().getClientId());
+		tokenServices.setRestTemplate(oAuth2RestTemplate);
+		oAuth2ClientAuthenticationFilter.setTokenServices(tokenServices);
+		return oAuth2ClientAuthenticationFilter;
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook")
+	public ClientResources facebook() {
+		return new ClientResources();
+	}
+	
+	@Bean
+	@ConfigurationProperties("google")
+	public ClientResources google() {
+		return new ClientResources();
+	}
+	
+	@Bean
+	@ConfigurationProperties("github")
+	public ClientResources github() {
+		return new ClientResources();
+	}
+	
+	@Configuration
+	@EnableResourceServer
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
+		}
+	}
+	
+	
 
 }
 
-*/
+class ClientResources {
+
+	@NestedConfigurationProperty
+	private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+	@NestedConfigurationProperty
+	private ResourceServerProperties resource = new ResourceServerProperties();
+
+	public AuthorizationCodeResourceDetails getClient() {
+		return client;
+	}
+
+	public ResourceServerProperties getResource() {
+		return resource;
+	}
+}
